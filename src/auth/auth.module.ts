86263d11f87300228d5f0bd77260259225ffe7ee
berkbbd <berk@bbdtech.co>
2023-10-19 15:24:50 +0300

    update.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,18 @@ import { AuthController } from './auth.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UsersRepository } from './users.repository';
+import {PassportModule} from "@nestjs/passport";
+import {JwtModule} from "@nestjs/jwt";
 
 @Module({
   imports: [
+      PassportModule.register({ defaultStrategy: 'jwt' }),
+      JwtModule.register( {
+         secret: 'top-secret',
+         signOptions: {
+             expiresIn: 3600
+         },
+      }),
       TypeOrmModule.forFeature([User])
   ],
   providers: [AuthService, UsersRepository],
