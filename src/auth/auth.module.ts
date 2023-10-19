bf75c7f7d3ab54cfc3dbdcdf092aab4848de0fe2
berkbbd <berk@bbdtech.co>
2023-10-19 17:09:47 +0300

    Configuration management is updated.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -17,12 +17,14 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         JwtModule.registerAsync( {
             imports: [ConfigModule],
             inject: [ConfigService],
-            useFactory: async (configService: ConfigService) => ({
-                secret: configService.get('JWT_SECRET'),
-                signOptions: {
-                    expiresIn: 3600
+            useFactory: async (configService: ConfigService) => {
+                return {
+                    secret: configService.get('JWT_SECRET'),
+                    signOptions: {
+                        expiresIn: 3600
+                    }
                 }
-            })
+            }
         }),
         TypeOrmModule.forFeature([User])
     ],
